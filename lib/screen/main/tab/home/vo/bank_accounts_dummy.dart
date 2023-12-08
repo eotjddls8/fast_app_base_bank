a4c51ed5fcb2eb16b929684bebeaa908787674bf
Lee j c 59 <eotjddls902@gmail.com>
2023-12-09 00:33:19 +0900

    second commit

---
+++ lib/screen/main/tab/home/vo/bank_accounts_dummy.dart
@@ -0,0 +1,25 @@
+import 'package:fast_app_base/screen/main/tab/home/banks_dummy.dart';
+import 'package:fast_app_base/screen/main/tab/home/vo/vo_bank_account.dart';
+
+final bankAccountShinhan1 =
+    BankAccount(bankShinhan, 3000000, accountTypeName: "신한 주거래 통장");
+final bankAccountShinhan2 =
+    BankAccount(bankShinhan, 30000000, accountTypeName: "저축예금");
+final bankAccountShinhan3 =
+    BankAccount(bankShinhan, 300000000, accountTypeName: "저축예금");
+final bankAccountToss = BankAccount(bankToss, 200000000);
+final bankAccountKakao =
+    BankAccount(bankKakao, 30000000000, accountTypeName: "입출금 통장");
+
+main(){
+  print(bankAcconts[1].accountTypeName);
+}
+
+
+final bankAcconts = [
+  bankAccountShinhan1,
+  bankAccountShinhan2,
+  bankAccountShinhan3,
+  bankAccountToss,
+  bankAccountKakao
+];
---
+++ lib/screen/main/tab/payment/f_payment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FaymentFragment extends StatefulWidget {
+  const FaymentFragment({super.key});
+
+  @override
+  State<FaymentFragment> createState() => _FaymentFragmentState();
+}
+
+class _FaymentFragmentState extends State<FaymentFragment> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
